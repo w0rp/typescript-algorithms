4b01a7c91acff4502ee28bab72275c9bacdb073e
w0rp <devw0rp@gmail.com>
2019-06-07 17:33:56 +0100

    Disable some jest features when running tests locally

--- jest.config.js
+++ jest.config.js
@@ -6,25 +6,34 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   testRegex: '\\.spec\\.ts$',
-  reporters: [
-    'jest-dot-reporter',
-    [
-      'jest-html-reporter',
-      {
-        pageTitle: 'Test Report',
-        outputPath: path.resolve(__dirname, '.report', 'jest.html'),
-      },
-    ],
-  ],
-  collectCoverage: true,
-  coverageDirectory: path.resolve(__dirname, '.report', 'coverage'),
-  coverageReporters: ['text', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+  reporters: ['jest-dot-reporter'],
+}
+
+// Enable HTML reports if an environment variable is set.
+if (process.env.HTML_REPORT === '1') {
+  module.exports.reporters.push([
+    'jest-html-reporter',
+    {
+      pageTitle: 'Test Report',
+      outputPath: path.resolve(__dirname, '.report', 'jest.html'),
     },
-  },
+  ])
+}
+
+// Enable coverage if an environment variable is set for it.
+// This makes it easy to run tests in editors without running coverage reports.
+if (process.env.COVERAGE === '1') {
+  Object.assign(module.exports, {
+    collectCoverage: true,
+    coverageDirectory: path.resolve(__dirname, '.report', 'coverage'),
+    coverageReporters: ['text', 'html'],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+  })
 }
