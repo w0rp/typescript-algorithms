239a4d9e5185d158d0f926d3e4144995f2e08880
w0rp <devw0rp@gmail.com>
2019-06-07 14:30:53 +0100

    Reformat: it fits on fewer lines

--- src/not-null.ts
+++ src/not-null.ts
@@ -2,11 +2,10 @@
  * Given any type and an optional message, throw a TypeError if the value is
  * null or undefined, or otherwise return the value.
  */
-export const notNull =
-  <T>(x: T | null | undefined, message?: string): T => {
-    if (x == null) {
-      throw new TypeError(message)
-    }
-
-    return x
+export const notNull = <T>(x: T | null | undefined, message?: string): T => {
+  if (x == null) {
+    throw new TypeError(message)
   }
+
+  return x
+}
